6740bcd28019040c173a1dbcea5ac090efd67528
Santiago Andres Alzate Serna <santiagoalzate93@gmail.com>
2018-10-10 00:06:28 -0500

    SQL

--- tables.sql
+++ tables.sql
@@ -1,21 +1,28 @@
+CREATE TABLE `BLOG_USUARIO` (
+  `login` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `nombre` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `apellido` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci
 
-CREATE TABLE `PACIENTES` (
-  `id` int(11) NOT NULL,
-  `tipo_id` varchar(2),
-  `nombre` varchar(45) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  PRIMARY KEY (`id`,`tipo_id`)
-) ENGINE=InnoDB;
+CREATE TABLE `BLOG` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `title` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `content` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `BLOG_BLOG_USUARIO_FK` (`author`),
+  CONSTRAINT `BLOG_BLOG_USUARIO_FK` FOREIGN KEY (`author`) REFERENCES `BLOG_USUARIO` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci
 
-
-
-CREATE TABLE `CONSULTAS` (
-  `idCONSULTAS` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha_y_hora` datetime NOT NULL,
-  `resumen` text  NOT NULL,
-  `PACIENTES_id` int(11) NOT NULL DEFAULT '0',
-  `PACIENTES_tipo_id` varchar(2),
-  PRIMARY KEY (`idCONSULTAS`),
-  KEY `fk_CONSULTAS_PACIENTES1` (`PACIENTES_id`,`PACIENTES_tipo_id`),
-  CONSTRAINT `fk_CONSULTAS_PACIENTES1` FOREIGN KEY (`PACIENTES_id`, `PACIENTES_tipo_id`) REFERENCES `PACIENTES` (`id`, `tipo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB;
+CREATE TABLE `BLOG_COMMENT` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `blog_id` int(11) NOT NULL,
+  `content` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `BLOG_COMMENT_BLOG_FK` (`blog_id`),
+  KEY `BLOG_COMMENT_BLOG_USUARIO_FK` (`author`),
+  CONSTRAINT `BLOG_COMMENT_BLOG_FK` FOREIGN KEY (`blog_id`) REFERENCES `BLOG` (`id`),
+  CONSTRAINT `BLOG_COMMENT_BLOG_USUARIO_FK` FOREIGN KEY (`author`) REFERENCES `BLOG_USUARIO` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci
